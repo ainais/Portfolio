c289c3afce861c9c5fa3c2dab49eba78db4ca737
Lee JeongSeong <kou8148@naver.com>
2023-03-03 00:56:20 +0900

    [JSLee][Fast-Api] Add Sample Test Project With Fast-Api

---
+++ Fast-Api/test/main.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def read_root():
+    return {"Hello" : "World2"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
